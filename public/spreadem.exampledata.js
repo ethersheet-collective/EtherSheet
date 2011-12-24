4c1dab98042fbd1117126456ae65cc266b356080
cooperq <cooper@radicaldesigns.org>
2011-12-24 15:47:01 -0800

    moving things into public dir

---
+++ public/spreadem.exampledata.js
@@ -0,0 +1,19 @@
+spreadem.sheet_values = {
+  0: {
+    1: { col: 1, guid: 1, val: 1 },
+    2:{ col: 2, guid: 2, val: 1 },
+    4:{ col: 4, guid: 3, val: 1 }
+  },
+  2:{
+    2: { col: 2, guid: 4, val: 1 },
+    3: { col: 3, guid: 5, val: 1 },
+    5: { col: 5, guid: 6, val: 1 }
+  },
+  3:{
+    0: { col: 0, guid: 7, val: 1 },
+    3: { col: 3, guid: 8, val: 1 },
+    4: { col: 4, guid: 9, val: 1 }
+  }
+};
+spreadem.row_size = 4;
+spreadem.col_size = 6;
