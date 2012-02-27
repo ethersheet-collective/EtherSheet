8fa58acfc10a28aca5f78f8c97fda2d0f43242d2
cooperq <cooper@radicaldesigns.org>
2012-02-27 12:36:37 -0800

    adding formula and cell reference support

--- public/ethersheet.js
+++ public/ethersheet.js
@@ -14,6 +14,7 @@ var es = {
     $(this.table_selector).html(this.sheet_to_table());
   },
   sheet_to_table : function(){
+    var self = this;
     html = '';
     for(i=0; i < this.row_size; i++){
       //draw row
@@ -32,24 +33,35 @@ var es = {
       //fill in the cell
         var cell = $('#cell-'+r+'-'+ i + ' div');
         cell.attr('id', row[i].guid);
-        cell.append(row[i].val);
+        cell.append(es.parse_value(row[i].val));
       }
     }
 
+    $(this.table_selector +' td div').focus(function(){
+      var row = $(this).attr('row');
+      var col = $(this).attr('col');
+      if(self.sheet_values[row][col]) {
+        $(this).html(self.sheet_values[row][col].val)
+      }
+    });
+
     $(this.table_selector +' td div').blur(function(){
       if($(this).html() !=''){
-        es.add_value($(this).attr('row'), $(this).attr('col'), $(this).html());
+        var row = $(this).attr('row');
+        var col = $(this).attr('col');
+        es.add_value($(this).attr('col'), $(this).attr('row'), $(this).html());
+        $(this).html =  self.sheet_values[row][col].display_value
       }
     });
   },
-  add_value: function(row, column, value){
-    console.log('adding values ' + row + ' ' + column + ' ' + value);
+  add_value: function(column, row, value){
+    console.log('adding values ' + column + ' ' + row + ' ' + value);
     row = parseInt(row);
     column = parseInt(column);
     if(this.sheet_values[row] === undefined){
       this.sheet_values[row] = {}
     }
-    this.sheet_values[row][column] = {guid: _.uniqueId(Math.floor(Math.random() * 100000)), val: value}
+    this.sheet_values[row][column] = {guid: _.uniqueId(Math.floor(Math.random() * 100000)), val: value, display_value: this.parse_value(value)}
     if(column+1 >= this.col_size){
       this.col_size = column+2;
     }
@@ -85,11 +97,36 @@ var es = {
   /*takes in the contents of a cell, returns the value if its a function*/
   parse_value: function(value){
     // its not a function so return immediately
-    if(!value.char_at(0) == '=') {
+    if(value.charAt(0) == '=') {
+      var formula = value.slice(1)
+      formula = this.parse_cell_refs(formula)
+      return formula_parser.parse(formula);
+    } else {
       return value;
     }
+  },
 
+  parse_cell_refs: function(formula){
+    var cells = formula.match(/[a-z]+[0-9]+/gi)
+    _.each(cells, function(c){
+      //TODO
+      //get ref_cell object from index
+      var cell = es.ord_to_ary_idx(c, es.sheet_values);
+      //insert cell ID into 'reverse_dependencies' of cell obj
+      //insert ref_cell ID into 'dependencies' of cell obj
+      //str_rep index with cell value
+      formula = formula.replace(c, es.parse_value(cell.val));
+    });
+    return formula;
+  },
+
+  ord_to_ary_idx: function(str, ary){
+    var letters = str.match(/[a-z]+/gi)[0].split('')
+    var row_idx = parseInt(str.match(/[0-9]+/g)[0]) - 1
+    var col_idx = _.reduce(letters, function(memo, c){ return memo + c.toUpperCase().charCodeAt(0)-65}, 0);
+    return(ary[row_idx][col_idx]);
   }
+
 };
 
 //when row is clicked make contents editable
