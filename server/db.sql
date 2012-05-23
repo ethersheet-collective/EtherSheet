46def0c9d45136a84498df07ee9b82304e9e1737
cooperq <cooper@radicaldesigns.org>
2012-05-23 10:42:14 -0700

    moving things into root directory

--- server/db.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE ethersheet;
-GRANT ALL ON ethersheet.* TO 'ethersheet'@'localhost' IDENTIFIED BY 'ethersheet';
-CREATE TABLE sheets (sheetid VARCHAR(40), sheetdata MEDIUMTEXT, PRIMARY KEY (sheetid));
