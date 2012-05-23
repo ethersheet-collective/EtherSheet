7774b7ee48668d11a1478c6a1f47779c977eb5e2
cooperq <cooper@radicaldesigns.org>
2012-05-23 11:13:36 -0700

    updating schema

--- db.sql
+++ db.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE ethersheet;
 GRANT ALL ON ethersheet.* TO 'ethersheet'@'localhost' IDENTIFIED BY 'ethersheet';
 CREATE TABLE sheets (sheetid VARCHAR(40), sheetdata MEDIUMTEXT, PRIMARY KEY (sheetid));
+CREATE TABLE users (token VARCHAR(255), color VARCHAR(7), nickname VARCHAR(255), PRIMARY KEY (token));
