18c733914fe0952360629ccf47951681c1c0555a
cooperq <cooperq@cooperq.com>
2012-05-23 19:54:25 +0000

    fixing typo

--- db.sql
+++ db.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE ethersheet;
 USE ethersheet;
-GRANT ALL ON ethersheet.* TO 'ethersheet'@'localhost' IDENTIFIED BY 'qo_.StMgsWtU%uT@)<_co6.+585f+K';
+GRANT ALL ON ethersheet.* TO 'ethersheet'@'localhost' IDENTIFIED BY 'ethersheet';
 CREATE TABLE sheets (sheetid VARCHAR(40), sheetdata MEDIUMTEXT, PRIMARY KEY (sheetid));
 CREATE TABLE users (token VARCHAR(255), color VARCHAR(7), nickname VARCHAR(255), PRIMARY KEY (token));
