99fbc3c77fc28d991af594693d04f0e342f06060
cooperq <cooper@radicaldesigns.org>
2012-05-23 11:15:34 -0700

    example config file

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  port: 8080,
-  https_key: './lib/snakeoil.key',
-  https_cert: './lib/snakeoil.crt',
-  mysql_database: 'ethersheet',
-  mysql_user: 'ethersheet',
-  mysql_password: 'ethersheet',
-  mysql_host: 'localhost'
-}
