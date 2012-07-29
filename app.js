9be8f781d805dd3a18d58df8f8b6c9d446a4701d
tedsuo <ted@radicaldesigns.org>
2012-07-29 11:22:06 +0200

    change config file location based on NODE_ENV

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var config = require('./config');
+if(process.env.NODE_ENV == 'test'){
+  console.log('starting Ethersheet in test mode');
+  var config = require('./test/config-test');
+} else {
+  var config = require('./config');
+}
 var server = require('./lib/server.js').createServer(config);
