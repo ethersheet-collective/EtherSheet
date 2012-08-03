cbce6ede9824d06c493932787b8316276550e0e0
tedsuo <ted@radicaldesigns.org>
2012-08-03 23:17:52 +0200

    testing table creation

--- public/test/views/sheet.js
+++ public/test/views/sheet.js
@@ -3,13 +3,21 @@ describe('SheetView', function(){
   var sheet;
 
   before(function(){
-    sheet = new SheetView({
+    sheet = new ES.SheetView({
       el: document.getElementById('ethersheet'),
+      model: new ES.Sheet()
     });
+    sheet.render();
   });
 
   it('should render a table', function(){
-    sheet.render();
     $('#ethersheet table').length.should.not.be.empty;
+  });
+
+  it('by default, it should create a blank 20x100 table display', function(){
+    $('#ethersheet .table-row').length.should.equal(100);
+    $('#ethersheet .table-cell').length.should.equal(2000);
+    $('#ethersheet .column-header').length.should.equal(20);
+    $('#ethersheet .row-header').length.should.equal(100);
   });
 });
