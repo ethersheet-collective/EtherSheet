2f7ccc324126187778ec025e7e4f56c3ab352d3a
tedsuo <ted@radicaldesigns.org>
2012-08-29 12:17:17 +0200

    insertRow, deleteRow, insertCol, deleteCol

--- public/es/models/sheet.js
+++ public/es/models/sheet.js
@@ -39,6 +39,35 @@ ES.Sheet = Backbone.Model.extend({
   colExists: function(col_id){
     return _.include(this.cols,col_id);
   },
+  insertRow: function(position){
+    var new_id = ES.uid();
+    this.rows.splice(position,0,new_id);
+    return new_id;
+  },
+  deleteRow: function(row_id){
+    var row_pos = _.indexOf(this.rows,row_id);
+    if(row_pos === -1) return false;
+    this.cells[row_id] = {};
+    this.rows.splice(row_pos,1);
+    return true;
+  },
+  insertCol: function(position){
+    var new_id = ES.uid();
+    this.cols.splice(position,0,new_id);
+    return new_id;
+  },
+  deleteCol: function(col_id){
+    var es = this;
+    var col_pos = _.indexOf(this.cols,col_id);
+    if(col_pos === -1) return false;
+    _.each(this.rows,function(row_id){
+      if(es.cells[row_id]){
+         delete es.cells[row_id][col_id];
+      }
+    });
+    this.cols.splice(col_pos,1);
+    return true;
+  },
   updateCell: function(row_id,col_id,value){
     if(!this.rowExists(row_id)) return false;
     if(!this.colExists(col_id)) return false;
