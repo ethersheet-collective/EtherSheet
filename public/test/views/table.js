ab6c2075ad7c2b57bc21069e6df3d207afb99189
tedsuo <ted@radicaldesigns.org>
2012-09-03 16:10:39 -0700

    cleaning up table test to work in node

--- public/test/views/table.js
+++ public/test/views/table.js
@@ -6,14 +6,19 @@ var TableView = require('es/views/table');
 var ES = require('es/config');
 var expect = require('chai').expect;
 var should = require('chai').should();
+var $ = require('jquery');
+
+// setup dom attachment point
+var $container = $('<div id="ethersheet-container" style="display:none;"></div').appendTo('body');
 
 describe('TableView', function(){
 
-  var table;
+  var table, $el;
 
   before(function(){
+    $el = $('<div id="ethersheet"></div>').appendTo('#ethersheet-container');
     table = new TableView({
-      el: document.getElementById('ethersheet'),
+      el: $el,
       model: new Sheet()
     });
     table.render();
@@ -23,14 +28,22 @@ describe('TableView', function(){
     $('#ethersheet table').length.should.not.be.empty;
   });
 
-  it('by default, it should create a blank 20x100 table display', function(){
-    var expected_cell_count = ES.DEFAULT_ROW_COUNT * ES.DEFAULT_COL_COUNT;
-    $('#ethersheet .table-cell').length.should.equal(expected_cell_count);
-    $('#ethersheet .table-row').length.should.equal(ES.DEFAULT_ROW_COUNT);
-    $('#ethersheet .column-header').length.should.equal(ES.DEFAULT_COL_COUNT);
-    $("#ethersheet .column-header").last().text().should.equal('T');
-    $('#ethersheet .row-header').length.should.equal(ES.DEFAULT_ROW_COUNT)
-    $("#ethersheet .row-header").last().text().should.equal('100');
+  describe('by default, it should create a blank 20x100 table display', function(){
+    it('should have the right number of cells', function(){
+      var expected_cell_count = ES.DEFAULT_ROW_COUNT * ES.DEFAULT_COL_COUNT;
+      $('#ethersheet .table-cell').length.should.equal(expected_cell_count);
+    });
+    it('should have the right number of rows', function(){
+      $('#ethersheet .table-row').length.should.equal(ES.DEFAULT_ROW_COUNT);
+    });
+    it('should have the right column headers', function(){
+      $('#ethersheet .column-header').length.should.equal(ES.DEFAULT_COL_COUNT);
+      $("#ethersheet .column-header").last().text().should.equal('T');
+    });
+    it('should have the right row headers', function(){
+      $('#ethersheet .row-header').length.should.equal(ES.DEFAULT_ROW_COUNT)
+      $("#ethersheet .row-header").last().text().should.equal('100');
+    });
   });
 });
 
