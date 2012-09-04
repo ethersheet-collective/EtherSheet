75439f1a215480a409f931046546e3cc03a77b0c
tedsuo <ted@radicaldesigns.org>
2012-09-03 23:22:58 -0700

    remove redundant test.sql, point travis.yml at correct files

--- lib/test/test.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE ethersheet;
-USE ethersheet;
-GRANT ALL ON ethersheet.* TO 'ethersheet'@'localhost' IDENTIFIED BY 'ethersheet';
-CREATE TABLE sheets (sheetid VARCHAR(40), sheetdata MEDIUMTEXT, PRIMARY KEY (sheetid));
-CREATE TABLE users (user_id VARCHAR(255), color VARCHAR(7), nickname VARCHAR(255), PRIMARY KEY (user_id));
