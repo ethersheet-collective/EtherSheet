5aaea01f07519d8cf514206af560628e56b7c14f
tedsuo <ted@radicaldesigns.org>
2012-09-03 23:06:39 -0700

    updated to express 3.0

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var config, server;
 
 if(process.env.NODE_ENV == 'test'){
   console.log('starting Ethersheet in test mode');
-  config = require('./test/config-test');
+  config = require('./lib/test/config-test');
 } else if(process.env.CONFIG_PATH){
   config = require(process.env.CONFIG_PATH);
 } else {
   config = require('./config');
 }
 
-server = require('./lib/server.js').createServer(config);
+server = require('./lib/node_modules/server.js').createServer(config);
