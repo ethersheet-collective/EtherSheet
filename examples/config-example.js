c5c326f26dacc7666815ff40fc35906a2e7a0c80
ethersheet-collective <ethersheet@ethersheet.org>
2012-12-18 23:05:01 +0000

    switched to using uberdb for ORM

--- examples/config-example.js
+++ examples/config-example.js
@@ -2,9 +2,12 @@ module.exports = {
   port: 8080,
   https_key: './examples/snakeoil.key',
   https_cert: './examples/snakeoil.crt',
-  mysql_database: 'ethersheet',
-  mysql_user: 'ethersheet',
-  mysql_password: 'ethersheet',
-  mysql_host: 'localhost',
-  debug: false
+  db_name: 'ethersheet',
+  db_user: 'ethersheet',
+  db_password: 'ethersheet',
+  db_host: 'localhost',
+  db_type: 'mysql',
+  debug: false,
+  default_row_count: 100,
+  default_col_count: 20
 }
--- test/unit/ethersheet.js
+++ test/unit/ethersheet.js
@@ -2,6 +2,7 @@ var EtherSheetService = require('../../lib/ethersheet').EtherSheetService;
 var _ = require('underscore');
 var config = require('../../config.js');
 var should = require('chai').should();
+var async = require('async');
 
 describe('EtherSheetService', function(){
 
@@ -18,119 +19,70 @@ describe('EtherSheetService', function(){
       var es = new EtherSheetService(config);
       var colors = [];
       for(var i=0; i<100; i++){
-        colors.push(es.get_random_color());
+        colors.push(es.getRandomColor());
       }
       _.uniq(colors).length.should.eql(EtherSheetService.colors.length);
        
     })
   })
 
-  describe('.sql', function(){
-    it("should have a sql connection", function(){
-      var es = new EtherSheetService(config);
-      es.should.have.property('sql');
-      es.sql.should.have.property('query');
-    })
-  })
-
-  describe('users', function(){
-    var es = new EtherSheetService(config);
-    it('should delete a user', function(done){
-      es.delete_user('test',function(err, results){
-        if(err){
-          throw err;
-        }
-        results.should.have.property('affectedRows');
-        results.affectedRows.should.be.below(2);
-        done();
-      });
-    });
-
-    it('should create a user', function(done){
-      es.find_or_create_user('test', function(err, user){
-        user.should.have.property('color');
-        user.should.have.property('nickname');
-        user.should.have.property('user_id');
-        user.user_id.should.eql('test');
-        user.color.should.not.be.false;
-        done();
-      });
-    });
-
-    it('should find a user', function(done){
-      es.find_or_create_user('test', function(err, user, results){
-        user.should.have.property('user_id');
-        results.length.should.eql(1);
-        done();
-      });
-    });
-  })
-
   describe('sheets', function(){
-    var es = new EtherSheetService(config);
+    var es;
+    
+    before(function(done){
+      es = new EtherSheetService(config);
+      es.onConnect(done);
+    });
+
     it('should delete a sheet', function(done){
-      es.delete_sheet('test',function(err, results){
-        if(err){
-          throw err;
-        }
-        results.should.have.property('affectedRows');
-        results.affectedRows.should.be.below(2);
-        done();
+      es.createSheet('delete_test',function(err){
+        if(err) throw err;
+        es.deleteSheet('delete_test',function(err){
+          if(err) throw err;
+          es.getRowIndex('delete_test',function(err,rows){
+            if(err) throw err;
+            should.not.exist(rows);
+            done();
+          });
+        });
       });
     });
 
-    it('should create a sheet', function(done){
-      es.find_or_create_sheet('test', function(err, sheet){
+    it('should create a sheet if it does not exist', function(done){
+      es.getSheet('create_test', function(err, sheet){
         if(err){
           throw err;
         }
-        sheet.should.have.property('sheetid');
-        sheet.should.have.property('sheetdata');
-        sheet.sheetid.should.eql('test');
-        sheet.sheetdata.should.eql = '';
+        sheet.should.have.property('row_index');
+        sheet.should.have.property('col_index');
+        sheet.should.have.property('cells');
+        sheet.cells.should.be.empty;
+        sheet.row_index.length.should.eq(config.default_row_count);
+        sheet.col_index.length.should.eq(config.default_col_count);
         done();
       });
     });
     
     it('should find a sheet', function(done){
-      es.find_or_create_sheet('test', function(err, sheet, results){
+      var cb = function(){};
+      async.parallel([
+        function(cb){es.setRowIndex('test_sheet',['foo'],cb)},
+        function(cb){es.setColIndex('test_sheet',['bar'],cb)}, 
+        function(cb){es.setCells('test_sheet',{foo:{bar:123}},cb)},
+      ], 
+      function(err){
         if(err){
           throw err;
         }
-        sheet.should.have.property('sheetid');
-        results.length.should.eql(1);
-        done();
+        es.getSheet('test_sheet', function(err, sheet){
+          if(err){
+            throw err;
+          }
+          sheet.cells.should.not.be.empty;
+          sheet.cells['foo']['bar'].should == 123;
+          done();
+        });
       });
     });
-  })
-
-  describe('adding and removing user from rooms', function(){
-    var es = new EtherSheetService(config);
-    var test_sheet = '';
-    var test_user = '';
-    before(function(done){
-      es.find_or_create_sheet('test', function(err, sheet){
-        test_sheet = sheet;
-      });
-      es.find_or_create_user('test', function(err, user){
-        test_user = user; 
-      });
-      done();
-    })
-
-    it('should add a user to a room', function(){
-      EtherSheetService.sheets.should.be.a('object');
-      es.add_user_to_room(test_user, test_sheet.sheetid, function(){
-        EtherSheetService.sheets.should.have.property(test_sheet.sheetid);
-        EtherSheetService.sheets[test_sheet.sheetid].count.should.eql(1);
-        EtherSheetService.sheets[test_sheet.sheetid].users.should.have.property(test_user.user_id);
-      });
-    });
-
-    it('should remove a user from a room', function(){
-      es.remove_user_from_room(test_user, test_sheet.sheetid);
-      EtherSheetService.sheets.should.not.have.property(test_sheet.sheetid);
-    });
-  })
-
-})
+  });
+});
